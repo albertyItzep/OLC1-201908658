5a212d042400a43f8572c42e8d14cb342ae9851e
albertyItzep <alberttitzep123@gmail.com>
2022-08-28 01:55:37 -0600

    Inicializando el proyecto1

---
+++ proyecto1/src/Views/HomeView.java
@@ -0,0 +1,11 @@
+package Views;
+
+import javax.swing.*;
+
+public class HomeView extends JFrame{
+    public JLabel newLabel;
+    public HomeView(){
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+}
---
+++ proyecto1/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+
+import Views.HomeView;
+
+public class Main {
+    public static void main(String[] args){
+        HomeView newHome = new HomeView();
+        newHome.show();
+    }
+}
